34d34ff432d3ed51200cfd9ee3f2cab7ec1c188f
Kevin Boston <kbos22@gmail.com>
2020-08-03 14:10:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ class App < Sinatra::Base
     erb :index 
   end
   post '/checkout' do 
-    @session[:item] = params[:item] 
-    
+    session[:item] = params[:item] 
+    @session = session
     
     erb :checkout
 end
